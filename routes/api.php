6ca6d2363fa5adb7861913c0b5b0bb17bcfe93ac
DarioLoForti <lofortidario@gmail.com>
2024-03-08 16:45:07 +0100

    inserisco nuova rotta nel file api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController as ProjectController;
+use App\Http\Controllers\Api\LeadController as LeadController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,6 +17,8 @@ use App\Http\Controllers\Api\ProjectController as ProjectController;
 
 Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('/projects/{slug}', [ProjectController::class, 'show']);
+
+Route::post('/contacts', [LeadController::class, 'store']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
